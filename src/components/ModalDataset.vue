dbbf69e4990ef459eae742379ff9799c14f75dfa
dottyz <yizhao.tan@gmail.com>
2019-06-10 15:00:00 -0400

    Modal to show dataset metadata

---
+++ src/components/ModalDataset.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <div>
+    <sui-button basic
+      type="button"
+      content="Show Data"
+      icon="right arrow"
+      label-position="right"
+      @click="$emit('toggle')"/>
+    <sui-modal v-model="open">
+      <sui-modal-header>{{ data !== null ? data.title : '' }}</sui-modal-header>
+      <sui-modal-content scrolling>
+        <sui-modal-description>
+          <sui-table celled>
+            <sui-table-header>
+              <sui-table-row>
+                <sui-table-header-cell>Field</sui-table-header-cell>
+                <sui-table-header-cell>Value</sui-table-header-cell>
+              </sui-table-row>
+            </sui-table-header>
+            <sui-table-body>
+              <sui-table-row v-for="(value, name) in data">
+                <sui-table-cell>
+                  <sui-label>{{ name }}</sui-label>
+                </sui-table-cell>
+                <sui-table-cell>{{ value }}</sui-table-cell>
+              </sui-table-row>
+            </sui-table-body>
+          </sui-table>
+        </sui-modal-description>
+      </sui-modal-content>
+      <sui-modal-actions>
+        <sui-button negative @click="$emit('toggle')">Cancel</sui-button>
+        <sui-button primary @click="$emit('submit')">Submit</sui-button>
+      </sui-modal-actions>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalExample',
+  props: {
+    data: Object,
+    open: Boolean
+  }
+}
+</script>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div is="sui-container">
-    <sui-form v-on:submit.prevent="loadPackage">
+    <sui-form>
       <sui-grid divided="vertically">
         <sui-grid-row :columns="2">
           <sui-grid-column>
@@ -23,15 +23,9 @@
           </sui-grid-column>
         </sui-grid-row>
         <sui-grid-row centered>
-          <sui-button basic
-            type="submit"
-            content="Show Data"
-            icon="right arrow"
-            label-position="right"
-            v-bind:disabled="this.dataset.url === null"/>
+          <ModalDataset v-bind:data="data" v-bind:open="viewing" @toggle="toggle" @submit="replicate"/>
         </sui-grid-row>
       </sui-grid>
-
     </sui-form>
   </div>
 </template>
@@ -41,6 +35,8 @@ import FormAPIKey from '@/components/FormAPIKey.vue'
 import FormDataset from '@/components/FormDataset.vue'
 import FormInstance from '@/components/FormInstance.vue'
 
+import ModalDataset from '@/components/ModalDataset.vue'
+
 const axios = require('axios')
 
 export default {
@@ -48,22 +44,26 @@ export default {
   components: {
     FormAPIKey,
     FormDataset,
-    FormInstance
+    FormInstance,
+    ModalDataset
   },
   methods: {
-    loadPackage: function () {
+    load: function () {
       // TODO: assert if urls are URL and no missing
       this.validate()
 
-      const link = this.dataset.url.origin + '/api/3/action/package_show?id=' + this.dataset.url.pathname.split('/')[2]
       axios.get(
-        link
+        this.dataset.url.origin + '/api/3/action/package_show?id=' + this.dataset.url.pathname.split('/')[2]
       ).then(
         response => {
           this.data = response.data.result
         }
       )
     },
+    replicate: function () {
+
+    },
+    // TODO: merge set functions
     setAPIKey: function (val, inst) {
       this[inst].key = val
     },
@@ -73,6 +73,14 @@ export default {
     setInstance: function (val) {
       this.instance.url = val
     },
+    toggle: function () {
+      if (!this.viewing) { // before show - always load the dataset (ie. changes to the dataset input not tracked)
+        // TODO: spin wheel while loading on the button before showing modal
+        this.load()
+      }
+
+      this.viewing = !this.viewing
+    },
     validate: function () {
       this.error = true
       // duplicate = this.dataset.url && this.instance.url && this.dataset.url.host === this.instance.url.host
@@ -80,8 +88,12 @@ export default {
   },
   data () {
     return {
+      // Package data
       data: null,
-      error: false,
+      // Track state
+      failing: false,
+      viewing: false,
+      // Form inputs
       dataset: {
         url: '',
         key: ''
@@ -89,7 +101,7 @@ export default {
       instance: {
         url: new URL('https://ckanadmin.prod-toronto.ca'),
         key: ''
-      }
+      },
     }
   }
 }
